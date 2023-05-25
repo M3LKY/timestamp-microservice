9c50148efd3744ecdb83ad508f1af7f6f7a5750d
M3LKY <melquisedecgonzalez74@gmail.com>
2023-05-25 13:33:51 -0400

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +18,36 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/:date?", function (req, res) {
+  var inputDate = req.params.date;
+  
+  // Check if inputDate is empty
+  if (!inputDate) {
+    var currentDate = new Date();
+    return res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+  }
+  
+  var date;
+  
+  // Check if the inputDate is a valid Unix timestamp
+  if (!isNaN(inputDate) && isFinite(inputDate)) {
+    date = new Date(parseInt(inputDate));
+  } else {
+    date = new Date(inputDate);
+  }
+  
+  // Check if the date is invalid
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+  
+  return res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000);
 });
